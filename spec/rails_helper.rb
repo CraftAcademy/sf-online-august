eef856c0b112b7221298bf62587e51cbe4aaf651
Amber <amber@amberwilkie.com>
2016-09-27 09:49:15 +0200

    Install cucumber

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,11 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 end
 
 Shoulda::Matchers.configure do |config|
