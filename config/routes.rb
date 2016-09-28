c796031b8440c0674cf43b93b6985406c7add3b3
Amber <amber@amberwilkie.com>
2016-09-28 18:14:48 +0200

    Allow my teammates the joy of creating their own routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/restaurant', controller: :restaurant, action: :index
 
-  resources :dishes, only: [:new, :show, :create, :edit, :destroy]
+  resources :dishes, only: [:new, :show, :create]
 
   get '/menu', controller: :menu, action: :index
 
