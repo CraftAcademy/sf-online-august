7e0a972f02817b472235a06390d495cadf142ae3
Amber <amber@amberwilkie.com>
2016-09-28 12:15:04 +0200

    Refactor out redirect from create Dish method

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -8,8 +8,9 @@ class DishesController < ApplicationController
   def create
     @dish = Dish.new(dish_name: params[:dish][:dish_name], dish_desc: params[:dish][:dish_desc], dish_price: params[:dish][:dish_price], dish_allergy: params[:dish][:dish_allergy], dish_ingredients: params[:dish][:dish_ingredients], dish_cal: params[:dish][:dish_cal])
     if @dish.save
-      redirect_to dish_path(@dish)
+      render :show
     else
+      # Display flash message
       redirect_to new_dish_path
     end
   end
