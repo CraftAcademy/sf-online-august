0ba34705d2f6818173d502025e30c00ab786a9ce
Amber <amber@amberwilkie.com>
2016-09-28 11:11:23 +0200

    Require Dishes to have a name and a price

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
-  describe 'Regression validations' do
+  describe 'DB table' do
     it { is_expected.to have_db_column :dish_name }
     it { is_expected.to have_db_column :dish_desc }
     it { is_expected.to have_db_column :dish_price }
     it { is_expected.to have_db_column :dish_allergy }
     it { is_expected.to have_db_column :dish_cal }
     it { is_expected.to have_db_column :dish_ingredients }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :dish_name }
+    it { is_expected.to validate_presence_of :dish_price }
   end
 end
