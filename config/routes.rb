1dd426412b941750a2b08feab7e00d3449ee49f3
Amber <amber@amberwilkie.com>
2016-09-28 16:46:45 +0200

    Restrict dish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/restaurant', controller: :restaurant, action: :index
 
-  resources :dishes
+  resources :dishes, only: [:new, :show, :create, :edit, :destroy]
 
   get '/menu', controller: :menu, action: :index
 
