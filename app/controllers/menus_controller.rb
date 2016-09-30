c718f13513c45a8243e35093f8b0b7c75a1e2781
Amber <amber@amberwilkie.com>
2016-09-30 17:59:17 +0200

    Remove unnused 'show' method in menus controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -17,10 +17,6 @@ class MenusController < ApplicationController
     end
   end
 
-  def show
-    @menu = Menu.find(params[:id])
-  end
-
   private
   def menu_params
     params.require(:menu).permit(:title)
