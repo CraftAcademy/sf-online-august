7ad50b19b4b379532907deef08bb2265182cf267
Amber <amber@amberwilkie.com>
2016-10-03 16:59:53 +0200

    Geocode newly-created restaurants Adjust factory to have an actual address

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,11 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-
+  geocoded_by :full_address
+  after_validation :geocode
   validates_presence_of :user, :name, :street, :zipcode, :town
+
+  private
+  def full_address
+    [street, zipcode, town, 'Sweden'].compact.join(', ')
+  end
 end
