5018e4947588c11739aa2871b742c7c6ba038f0d
Amber <amber@amberwilkie.com>
2016-10-07 22:24:28 +0200

    Fix failing spec test

--- spec/helpers/restaurant_helper_spec.rb
+++ spec/helpers/restaurant_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RestaurantHelper do
 
   it "returns the restaurant categories, plus 'All Restaurants'" do
-    category_list = RestaurantHelper.dropdown_options
+    category_list = dropdown_options
     expect(category_list.length).to eq (Restaurant::PERMITTED_CATEGORIES.length + 1)
   end
 end
