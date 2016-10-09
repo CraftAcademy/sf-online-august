89234fab0ea17d9867abb33ac9aaa9a91d768b8b
Amber <amber@amberwilkie.com>
2016-10-09 11:53:51 +0200

    Add image to dish model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -6,4 +6,9 @@ class Dish < ApplicationRecord
 
   has_and_belongs_to_many :menus
   belongs_to :restaurant
+
+  has_attached_file :avatar,
+                    styles: { medium: "300x300>", thumb: "100x100>" },
+                    default_url: "assets/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
