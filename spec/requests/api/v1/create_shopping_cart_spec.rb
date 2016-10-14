cf6da2bc7422862024ef86d0381eb08127d914d5
Amber <amber@amberwilkie.com>
2016-10-14 13:12:59 +0200

    Take out hard-coded number from carts api spec test

--- spec/requests/api/v1/create_shopping_cart_spec.rb
+++ spec/requests/api/v1/create_shopping_cart_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Api::V1::CartsController do
     post '/api/v1/carts', {params: {dish_id: dish.id}}
 
     expect(ShoppingCart.count).to eq 1
-    expect(response_json).to eq({'cart_id' => 1, 'dish_id' => dish.id})
+    expect(response_json).to eq({'cart_id' => ShoppingCart.last.id, 'dish_id' => dish.id})
   end
 
   describe 'adding more dishes to cart' do
