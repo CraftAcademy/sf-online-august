cb6f9897f449bce18eefbabc1e5faf336595ab19
Amber <amber@amberwilkie.com>
2016-10-14 14:28:42 +0200

    Fix up formatting problems

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -11,12 +11,10 @@ class Api::V1::CartsController < ApplicationController
 
   def update
     @cart = ShoppingCart.find_or_create_by(id: params[:id])
-    begin
-      dish = Dish.find(params[:dish_id])
-      @cart.add(dish, dish.price)
-    rescue
-      render json: ({'error' => 'Dish ID was invalid'})
-    end
+    dish = Dish.find(params[:dish_id])
+    @cart.add(dish, dish.price)
+  rescue
+    render json: ({'error' => 'Dish ID was invalid'})
   end
 
 end
